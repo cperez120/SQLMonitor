f30fa8c8ec40d1ccaf47089bb6cfaa2404b249b8
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-03-25 09:16:29 +0530

    Updated more counters in Perfmon Template
    Updated more counters in Perfmon Template

--- NonSql-Files/perfmon-collector-logman.ps1
+++ NonSql-Files/perfmon-collector-logman.ps1
@@ -1,12 +1,15 @@
 ﻿cls
 
-$collector_root_directory = 'D:\MSSQL15.MSSQLSERVER\MSSQL\Perfmon';
+# Copy paste the template file on folder that would contain Perfmon data collection logs
+    # Point the template path
+$data_collector_template_path = “E:\Perfmon\DBA_PerfMon_All_Counters_Template.xml”;
 $data_collector_set_name = 'DBA';
-$data_collector_template_path = “$collector_root_directory\DBA_PerfMon_NonSQL_Collector_Template.xml”;
-$log_file_path = "$collector_root_directory\$($env:COMPUTERNAME)__"
+
+# find Perfmon data collection logs folder path
+$collector_root_directory = Split-Path $data_collector_template_path -Parent
+$log_file_path = "$collector_root_directory\$data_collector_set_name"
 $file_rotation_time = '04:00:00'
 $sample_interval = '00:00:05'
-$version_format = 'yyyyMMdd\-HHmmss'
 
 logman import -name “$data_collector_set_name” -xml “$data_collector_template_path”
 logman update -name “$data_collector_set_name” -f bin -cnf "$file_rotation_time" -o "$log_file_path" -si "$sample_interval"
