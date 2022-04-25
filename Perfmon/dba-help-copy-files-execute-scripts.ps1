19313e439ecef7364416b1334cc54f031751feba
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-04-24 23:00:09 -0700

    update grafana variables

--- Perfmon/dba-help-copy-files-execute-scripts.ps1
+++ Perfmon/dba-help-copy-files-execute-scripts.ps1
@@ -1,17 +1,21 @@
-﻿$HostName = 'AllVersion'
-$DestinationDrive = 'Y:\'
+﻿$HostName = 'Demo'
+$DestinationDrive = 'C:\'
 
 # Copy Perfmon scripts
-Copy-Item -Path '\\SQLShare\S$\Perfmon' -Destination "\\$HostName\$($DestinationDrive.Replace(':','$'))" -Recurse -Container -Force
+Copy-Item -Path 'F:\GitHub\SqlServer-Baselining-Grafana\Perfmon' -Destination "\\$HostName\$($DestinationDrive.Replace(':','$'))" -Recurse -Container -Force
 
 $ssn = New-PSSession -ComputerName $HostName
 Invoke-Command -Session $ssn -ScriptBlock { 
+    Param ($Drive)
     # Set execution policy
     Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy Unrestricted -Force 
+
+    logman stop -name “DBA”
+    logman delete -name “DBA”
     
     # Create data collector
-    & 'Y:\Perfmon\perfmon-collector-logman.ps1' -TemplatePath 'Y:\Perfmon\DBA_PerfMon_All_Counters_Template.xml'
-}
+    & "$($Using:DestinationDrive)Perfmon\perfmon-collector-logman.ps1" -TemplatePath "$($Using:DestinationDrive)Perfmon\DBA_PerfMon_All_Counters_Template.xml"
+} 
 
 $ssn | Remove-PSSession
 
