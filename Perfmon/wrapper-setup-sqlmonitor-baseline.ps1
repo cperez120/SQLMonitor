6febd62e8b123bf27185763b960f88d91f839965
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-05-25 17:50:56 +0530

    updated code further

--- Perfmon/wrapper-setup-sqlmonitor-baseline.ps1
+++ Perfmon/wrapper-setup-sqlmonitor-baseline.ps1
@@ -1,26 +1,34 @@
-﻿#$adCredential = Get-Credential -UserName 'Lab\adwivedi' -Message 'SQL Admin'
-#$saCredential = Get-Credential -UserName 'sa' -Message 'sa'
-#$localAdmin = Get-Credential -UserName 'DEMO\Administrator' -Message 'Windows Admin'
-#Pa$$w0rd
+﻿#$LabCredential = Get-Credential -UserName 'Lab\SQLServices' -Message 'Angel Broking'
+#$saAdmin = Get-Credential -UserName 'sa' -Message 'sa'
+#$localAdmin = Get-Credential -UserName 'DEMO\Administrator' -Message 'Administrator'
+
 cls
 $params = @{
-    SqlInstanceToBaseline = 'Demo\SQL2014'
-    DbaToolsFolderPath = 'F:\GitHub\dbatools'
-    SqlCredential = $saCredential
-    WindowsCredential = $localAdmin
-    SkipSteps = @('11__CopyPerfmonFolder2Host','12__SetupPerfmonDataCollector','13__CreateJobCollectOSProcesses','14__CreateJobCollectPerfmonData')
-    #StartAtStep = '11__CopyPerfmonFolder2Host'
-    #StopAtStep = '11__CopyPerfmonFolder2Host'
+    SqlInstanceToBaseline = '192.168.56.31'
+    DbaDatabase = 'DBA_Admin'
+    DbaToolsFolderPath = 'D:\Softwares\dbatools'
+    InventoryServer = '192.168.56.15'
+    SqlCredential = $saAdmin
+    WindowsCredential = $LabCredential
+    #SkipSteps = @('11__CopyPerfmonFolder2Host','12__SetupPerfmonDataCollector','13__CreateJobCollectOSProcesses','14__CreateJobCollectPerfmonData','21__CreateJobUpdateSqlServerVersions','22__LinkedServerOnInventory')
+    #StartAtStep = '8__usp_GetAllServerInfo'
+    #StopAtStep = '22__GrafanaLogin'
 }
-F:\GitHub\SQLMonitor\Perfmon\setup-sqlmonitor-baseline.ps1 @Params
+D:\GitHub\SQLMonitor\Perfmon\setup-sqlmonitor-baseline.ps1 @Params #-Debug
 
+#Get-DbaDbMailProfile -SqlInstance '192.168.56.31' -SqlCredential $personalCredential
+#Copy-DbaDbMail -Source '192.168.56.15' -Destination '192.168.56.31' -SourceSqlCredential $personalCredential -DestinationSqlCredential $personalCredential # Lab
+#Remove-DbaDbMailProfile -SqlInstance '192.168.56.31' -SqlCredential $personalCredential -Profile @('Server Configuration','intranet','NXT')
+#Remove-DbaDbMailAccount -SqlInstance '192.168.56.31' -SqlCredential $personalCredential -Account @('intranet','NXT')
 <#
 
 Enable-PSRemoting -Force # run on remote machine
 Set-Item WSMAN:\Localhost\Client\TrustedHosts -Value * -Force # run on local machine
+Set-Item WSMAN:\Localhost\Client\TrustedHosts -Value 192.168.56.15 -Force
 #Set-NetConnectionProfile -NetworkCategory Private # Execute this only if above command fails
 
-$ssn = New-PSSession -ComputerName DEMO -Credential $localAdmin -Authentication Negotiate
-Test-WSMan DEMO -Credential $localAdmin -Authentication Negotiate
+Enter-PSSession -ComputerName '192.168.56.31' -Credential $localAdmin -Authentication Negotiate
+Test-WSMan '192.168.56.31' -Credential $localAdmin -Authentication Negotiate
+
 
 #>
