c0ea48b680e83c04f3e187be1610c245c6ead6bb
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-05-31 20:46:58 +0530

    added credential manager code

--- DDLs/SCH-[dbo].[credential_manager].sql
+++ DDLs/SCH-[dbo].[credential_manager].sql
@@ -14,6 +14,8 @@ create table dbo.credential_manager
 	created_by varchar(125) not null default suser_name(),
 	updated_date datetime2 not null default getdate(),
 	updated_by varchar(125) not null default suser_name(),
+	delegate_login_01 varchar(125) null,
+	delegate_login_02 varchar(125) null,
 	remarks nvarchar(2000),
 	dummy as 1/0,
 	constraint pk_credential_manager primary key clustered (server_ip, [user_name])
@@ -30,6 +32,8 @@ create or alter procedure dbo.usp_add_credential
 	@is_sql_user bit = 0,
 	@is_rdp_user bit = 0,
 	@save_passphrase bit = 1,
+	@delegate_login_01 varchar(125) = null,
+	@delegate_login_02 varchar(125) = null,
 	@remarks nvarchar(2000) = null
 with  encryption
 as
@@ -44,11 +48,11 @@ begin
 	end
 	
 	insert dbo.credential_manager
-	(server_ip, server_name, [user_name], password_hash, salt, is_sql_user, is_rdp_user, remarks)
+	(server_ip, server_name, [user_name], password_hash, salt, is_sql_user, is_rdp_user, delegate_login_01, delegate_login_02, remarks)
 	select server_ip = @server_ip, server_name = @server_name, [user_name] = @user_name,
 			password_hash = EncryptByPassPhrase(@passphrase_string, @password_string, 1, @server_ip),
 			salt = case when @save_passphrase = 0 then null else convert(varbinary(125),@passphrase_string) end,
-			is_sql_user = @is_sql_user, is_rdp_user = @is_rdp_user, remarks = @remarks;
+			is_sql_user = @is_sql_user, is_rdp_user = @is_rdp_user, @delegate_login_01, @delegate_login_02, remarks = @remarks;
 	
 
 	if @@ROWCOUNT > 0
@@ -79,26 +83,27 @@ create or alter procedure dbo.usp_get_credential
 with  encryption
 as
 begin
-	if @server_ip is null and @user_name is null and @server_name is null
+	set nocount on;
+	if (@server_ip is null and @user_name is null and @server_name is null) and (IS_SRVROLEMEMBER('SYSADMIN') <> 1)
 		throw 50000, 'Kindly provide both server_ip/server_name or user_name.', 1;
 
 	if IS_SRVROLEMEMBER('SYSADMIN') <> 1
-		print 'Since caller is not a sysadmin, Only look for credentials created/updated by caller.'
+		print 'Since caller is not a sysadmin, Only look for credentials created/updated by caller, or caller is delegate.'
 
 	if object_id('tempdb..#matched_credentials') is not null
 		drop table #matched_credentials;
 	select server_ip, server_name, [user_name], is_sql_user, is_rdp_user, 
 			password_hash, --[password] = cast(DecryptByPassPhrase(cast(salt as varchar),password_hash ,1, @server_ip) as varchar),
 			salt, --salt_raw = cast(salt as varchar),		
-			created_date, created_by, updated_date, updated_by, remarks
+			created_date, created_by, updated_date, updated_by, 
+			delegate_login_01, delegate_login_02, remarks
 	into #matched_credentials
 	from dbo.credential_manager
 	where (@server_ip is null or server_ip = @server_ip)
 	and (@server_name is null or server_name = @server_name)
 	and (@user_name is null or [user_name] = @user_name)
-	and (	IS_SRVROLEMEMBER('SYSADMIN') = 1
-		or
-			(created_by = SUSER_NAME() or updated_by = SUSER_NAME())
+	and (	(IS_SRVROLEMEMBER('SYSADMIN') = 1)
+		or	(created_by = SUSER_NAME() or updated_by = SUSER_NAME() or delegate_login_01 = SUSER_NAME() or delegate_login_02 = SUSER_NAME())
 		);
 
 	if(@passphrase_string is not null) and (select count(*) from #matched_credentials) > 1
@@ -113,8 +118,8 @@ begin
 		begin
 			print 'exact one match found. Decrypting password, and storing to output variable..';
 			select @password = case when @passphrase_string is null 
-										then cast(DecryptByPassPhrase(cast(salt as varchar),password_hash ,1, @server_ip) as varchar)
-										else cast(DecryptByPassPhrase(@passphrase_string,password_hash ,1, @server_ip) as varchar)
+										then cast(DecryptByPassPhrase(cast(salt as varchar),password_hash ,1, isnull(@server_ip,server_ip)) as varchar)
+										else cast(DecryptByPassPhrase(@passphrase_string,password_hash ,1, isnull(@server_ip,server_ip)) as varchar)
 										end
 			from #matched_credentials
 		end
@@ -135,14 +140,14 @@ end
 go
 
 -- Check all Logins
-select server_ip, server_name, [user_name], is_sql_user, is_rdp_user, 
-		password_hash, salt, created_date, created_by, updated_date, updated_by, remarks 
+select server_ip, server_name, [user_name], is_sql_user, is_rdp_user, password_hash, salt, 
+created_date, created_by, updated_date, updated_by, delegate_login_01, delegate_login_02, remarks 
 from dbo.credential_manager
 go
 
 -- Fetch password
 declare @password varchar(256);
-exec DBA_Inventory.dbo.usp_get_credential 
+exec DBA.dbo.usp_get_credential 
 		--@server_ip = '*',
 		--@user_name = 'Lab\SQLServices',
 		@password = @password output;
