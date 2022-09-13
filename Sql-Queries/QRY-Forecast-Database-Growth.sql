427439f9060f347fa94c5b09c090ec7911e210ea
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-09-13 22:49:41 +0530

    Updated Size Forcast Queries

--- Sql-Queries/QRY-Forecast-Database-Growth.sql
+++ Sql-Queries/QRY-Forecast-Database-Growth.sql
@@ -1,33 +1,44 @@
 use DBA
 go
 
+set nocount on;
 declare @host_name varchar(125);
 declare @database_name varchar(125);
 declare @object_name varchar(255);
+declare @sql nvarchar(max);
+declare @params nvarchar(max);
+declare @lfcr nchar(2) = nchar(13)+nchar(10);
+
+set @params = N'@host_name varchar(125), @database_name varchar(125)';
 
 select @host_name = host_name from dbo.instance_details;
 set @object_name = (case when @@SERVICENAME = 'MSSQLSERVER' then 'SQLServer' else 'MSSQL$'+@@SERVICENAME end);
-set @database_name = 'DBA'
+--set @database_name = 'DBA'
 
+set quoted_identifier off;
+set @sql = "
 ;with t_size_start as (
-	select top 1 'InitialSize' as QueryData, *
+	select 'InitialSize' as QueryData, [RowRank] = row_number()over(partition by instance order by collection_time_utc asc), *
 	from dbo.performance_counters pc
 	where pc.host_name = @host_name
-	and pc.object = (@object_name+':Databases') and pc.counter in ('Data File(s) Size (KB)')
-		 and pc.instance = @database_name
-	order by collection_time_utc asc
+	and pc.object = ('"+@object_name+":Databases') and pc.counter in ('Data File(s) Size (KB)')
+	"+(case when @database_name is null then '--' else '' end)+"and instance = @database_name
 )
 , t_size_latest as (
-	select top 1 'CurrentSize' as QueryData, *
+	select 'CurrentSize' as QueryData, [RowRank] = row_number()over(partition by instance order by collection_time_utc desc), *
 	from dbo.performance_counters pc
 	where pc.host_name = @host_name
-	and pc.object = (@object_name+':Databases') and pc.counter in ('Data File(s) Size (KB)')
-		 and pc.instance = @database_name
-	order by collection_time_utc desc
+	and pc.object = ('"+@object_name+":Databases') and pc.counter in ('Data File(s) Size (KB)')
+	"+(case when @database_name is null then '--' else '' end)+"and instance = @database_name
 )
-select 'Size-from-Start' as QueryData, i.collection_time_utc as start__collection_time_utc
-		,i.value/1024/1024 as [start__size_gb] ,l.value/1024/1024 as [current__size_gb]
+select 'Db-Size-from-Start' as QueryData, [Database] = l.instance, [start__collection_time_utc] = convert(smalldatetime,i.collection_time_utc)
+		,[start__size_gb] = convert(numeric(20,2),i.value/1024/1024) ,[current__size_gb] = convert(numeric(20,2),l.value/1024/1024)
 		,DATEDIFF(day,i.collection_time_utc, l.collection_time_utc) as [days-of-growth]
-		,(l.value-i.value)/1024.0/1024.0 as [size_gb-of-growth]
-		,(((l.value-i.value)/1024.0/1024.0)/(DATEDIFF(day,i.collection_time_utc, l.collection_time_utc)))*90 as [estimated-size_gb-for-90Days]
-from t_size_latest l, t_size_start i;
+		,[size_gb-of-growth] = convert(numeric(20,2),(l.value-i.value)/1024.0/1024.0)
+		,[estimated-size_gb-for-90Days] = convert(numeric(20,2),(((l.value-i.value)/1024.0/1024.0)/(DATEDIFF(day,i.collection_time_utc, l.collection_time_utc)))*90)
+from t_size_latest l join t_size_start i on l.instance = i.instance
+where l.RowRank = 1 and i.RowRank = 1";
+set quoted_identifier on;
+
+exec sp_executesql @sql, @params, @host_name, @database_name;
+go
--- Sql-Queries/QRY-Get-Database-Growth-Over-Time.sql
+++ Sql-Queries/QRY-Get-Database-Growth-Over-Time.sql
@@ -1,35 +1,57 @@
 use DBA
 go
 
+set nocount on;
+declare @host_name varchar(125);
+declare @database_name varchar(125);
 declare @object_name varchar(255);
 declare @sql nvarchar(max);
-declare @databases nvarchar(max);
+declare @params nvarchar(max);
+declare @lfcr nchar(2) = nchar(13)+nchar(10);
 
-set @object_name = (case when @@servicename = 'MSSQLSERVER' then 'SQLServer' else 'MSSQL$'+@@servicename end);
+set @params = N'@host_name varchar(125), @database_name varchar(125)';
+
+select @host_name = host_name from dbo.instance_details;
+set @object_name = (case when @@SERVICENAME = 'MSSQLSERVER' then 'SQLServer' else 'MSSQL$'+@@SERVICENAME end);
+--set @database_name = 'DBA'
 
 if object_id('tempdb..#FileSize') is not null drop table #FileSize;
-select [time] = convert(date,pc.collection_time_utc), instance, [counter]
-	--,object, counter, instance
-	,[size(kb)] = avg(pc.value)
-into #FileSize
-from dbo.vw_performance_counters pc with (nolock)
-where 1=1
---and pc.collection_time_utc between DATEADD(day,-5,getutcdate()) and getutcdate()
-and  pc.object = (@object_name+':Databases') 
-and pc.counter in ('Log File(s) Size (KB)',--'Log File(s) Used Size (KB)', 'Data File(s) Used Size (KB)',
-				'Data File(s) Size (KB)')
-group by convert(date,pc.collection_time_utc), instance, [counter];
+create table #FileSize ([Date] date not null, [Database] nvarchar(255) not null, [counter] varchar(255) not null, [SizeKB] numeric(20,2));
+
+set quoted_identifier off;
+set @sql = "with Sizing as (
+	select *, [RowRank] = row_number()over(partition by convert(date,pc.collection_time_utc), instance, counter order by collection_time_utc asc)
+	from dbo.vw_performance_counters pc with (nolock)
+	where 1=1
+	and pc.host_name = @host_name
+	and  pc.object = ('"+@object_name+":Databases') 
+	and pc.counter in ('Log File(s) Size (KB)', 'Data File(s) Size (KB)')
+	"+(case when @database_name is null then '--' else '' end)+"and instance = @database_name
+)
+select [Date] = convert(date,collection_time_utc), [Database] = instance, [counter], [SizeKB] = [value]
+from Sizing s
+where RowRank = 1
+"
+set quoted_identifier off;
+
+insert #FileSize ([Date], [Database], [counter], [SizeKB])
+exec sp_executesql @sql, @params, @host_name, @database_name;
+
+--select * from #FileSize order by 1, 2, 3;
 
 if object_id('tempdb..#DatabaseSize') is not null drop table #DatabaseSize;
-select [Date] = [time], [Database] = instance, 
+select [Date], [Database], 
 		[LogSize_gb] = ceiling([Log File(s) Size (KB)]/1024/1024), 
 		[DataSize_gb] = ceiling([Data File(s) Size (KB)]/1024/1024),
 		[TotalSize_gb] = ceiling(([Log File(s) Size (KB)]+[Data File(s) Size (KB)])/1024/1024)
 into #DatabaseSize
 from #FileSize up
-pivot ( max([size(kb)]) for [counter] in ([log file(s) size (kb)],[Data File(s) Size (KB)]) ) as pvt
+pivot ( max([SizeKB]) for [counter] in ([log file(s) size (kb)],[Data File(s) Size (KB)]) ) as pvt
 order by 1, 2;
 
+--select * from #DatabaseSize order by 1,2;
+
+declare @databases nvarchar(max);
 select @databases = coalesce(@databases + ', '+ quotename(name), quotename(name)) 
 from sys.databases order by name;
 
