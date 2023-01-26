47f4d6bbd6162147355e84bb60c963701d4b7c6b
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2023-01-26 16:43:10 +0530

    Updated table size query

--- Sql-Queries/QRY-SQLMonitor-Table-Size-Estimate.sql
+++ Sql-Queries/QRY-SQLMonitor-Table-Size-Estimate.sql
@@ -1,5 +1,10 @@
-select pt.table_name, pt.retention_days, bi.index_size_summary, bi.data_compression_desc
-from dbo.BlitzIndex bi join dbo.purge_table pt 
+use DBA_Admin
+go
+
+select table_name = coalesce(pt.table_name, 'dbo.'+bi.table_name), bi.index_name, pt.retention_days, 
+		bi.index_size_summary, bi.data_compression_desc
+from dbo.BlitzIndex bi left join dbo.purge_table pt 
 	on pt.table_name = 'dbo.'+bi.table_name
 where bi.run_datetime = (select max(run_datetime) from dbo.BlitzIndex i)
-and bi.database_name = 'DBA_Admin'
+and bi.database_name = DB_NAME()
+and (pt.table_name is not null or bi.table_name = 'WhoIsActive')
